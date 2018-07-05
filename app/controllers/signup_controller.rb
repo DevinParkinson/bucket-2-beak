2c55ed94bd3997ff9e4eb86af8985ebe39d091e5
Devin Parkinson <dparky13@icloud.com>
2018-07-05 10:20:26 -0600

    removing service worker

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,14 @@
 class SignupController < ApplicationController
-  def index
-  end
+    default from: 'buckettobeak@gmail.com'
+
+    def send_enabled_message(user)
+      @user = user
+      mail(:to => user.email, :subject => "Welcome to Bucket To Beak!!!")
+    end
+
+    def send_new_user_message(user)
+      @user = user
+      mail(:to => 'buckettobeak@gmail.com', :subject => "New User #{user} created please review and enable.")
+    end
+    
 end
