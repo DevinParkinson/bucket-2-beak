8147c1167d63ba1b2055e659bff3a28889f3cbe9
Devin Parkinson <dparky13@icloud.com>
2018-07-13 13:41:19 -0600

    moving to dev

--- config/initializers/mail.rb
+++
@@ -1,8 +0,0 @@
-begin
- mail = "#{Rails.root}/config/mail.yml"
- YAML.load_file(mail).each do |key, value|
-   ENV[key] = value
- end
-rescue
- raise "No mail.yml config file in your config directory."
-end
